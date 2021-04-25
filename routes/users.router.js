cdc5053544452e411ac69feb4bd90c5f92f0e4bb
Maddy@Xdev200 <xdev200200@gmail.com>
2021-04-25 17:02:35 +0530

    add:new routes

--- routes/users.router.js
+++ routes/users.router.js
@@ -6,9 +6,7 @@ router.route("/")
 .get(async(req,res)=>{
     try{
         const data = await Users.find({})
-        console.log(data)
         res.json({success:true,data})
-        console.log(data)
     }catch(err){
         res.status(500).json({success:false,message:"Unable to find user",errorMessage:err.message})
     }
@@ -19,10 +17,30 @@ router.route("/")
         const users = req.body;
         const NewUser = new Users(users)
         const savedUser = await NewUser.save()
-        localStorage.setItem("users",savedUser)
         res.json({success:true,users:savedUser})
     }catch(err){
         res.status(500).json({ success: false, message: "unable to register", errorMessage: err.message})
+    }
+})
+router.route("/:fb_id")
+.get(async(req,res)=>{
+    try{
+        const fb_id = req.params
+        const data = await Users.findOne({fb_id})
+        res.json({success:true,data})
+    }catch(err){
+        res.status(500).json({success:false,message:"Unable to find user using fb id",errorMessage:err.message})
+    }
+})
+
+.router.route("/:mobile")
+.get(async(req,res)=>{
+    try{
+        const mobile = req.params
+        const data = await.Users.findOne({mobile})
+        res.json({success:true,data})
+    }catch(err){
+        res.status(500).json({success:false,message:"Unable to find user using mobile no",errorMessage:err.message})
     }
 })
 
